b0c9eb96f05fdfe8389f68a8dd3fa2b697f427db
Aranjhonn <aranjhonn@yahoo.com>
2022-03-22 16:57:30 -0300

    pequena alteração

--- variaveis-e-input/app.js
+++ variaveis-e-input/app.js
@@ -7,8 +7,9 @@ const minhaPrimeiraConstanteString = 'Minha primeira constante'
 console.log(minhaPrimeiraConstanteString)
 
 let leituraDeCampo
+let teste = 'teste'
 readLine.question('Informe sua idade: ', input => {
     leituraDeCampo = input
-    console.log(`O usuário digitou: ${leituraDeCampo}`)
+    console.log(`O usuário digitou: ${leituraDeCampo} e a minha varável de teste é: ${teste}`)
 } )
 
